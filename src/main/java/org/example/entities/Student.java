17deb01188fe32145ec7271ed85ec4b8239d0f14
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-17 15:57:00 +0530

    Use of @GeneratedValue with strategy=table

--- src/main/java/org/example/entities/Student.java
+++ src/main/java/org/example/entities/Student.java
@@ -3,7 +3,7 @@ import jakarta.persistence.*;
 
 @Entity
 public class Student {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.TABLE)
     private long id;
 
     @Column(name = "student_name")
