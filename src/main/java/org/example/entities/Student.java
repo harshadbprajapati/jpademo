68501c5733b06f685a31bb3ffe7cdcdaac699c4c
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-17 09:19:28 +0530

    Use of hbm2ddl.auto=update

--- src/main/java/org/example/entities/Student.java
+++ src/main/java/org/example/entities/Student.java
@@ -3,11 +3,13 @@ import jakarta.persistence.*;
 
 @Entity
 public class Student {
-    @Id @GeneratedValue
+    @Id
     private long id;
 
     @Column(name = "student_name")
     private String studentName;
+
+//    private int rollNumber;
 
     public long getId() {
         return id;
@@ -24,84 +26,6 @@ public class Student {
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", studentName='" + studentName + '\'' +
-                '}';
-    }
-}
-
-//import jakarta.persistence.*;
-//
-//import java.util.Date;
-//
-//@Entity
-//public class Student {
-//
-//    @TableGenerator(name="studentIdGenerator", table = "student_id_generator",
-//            pkColumnName = "student_id", pkColumnValue = "student_id_value",
-//            allocationSize = 1
-//    )
-//    @Id @GeneratedValue(strategy = GenerationType.TABLE, generator = "studentIdGenerator")
-//    private long id;
-//
-//    @Column(name = "student_name")
-//    private String studentName;
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public String getStudentName() {
-//        return studentName;
-//    }
-//
-//    public void setStudentName(String studentName) {
-//        this.studentName = studentName;
-//    }
-//}
-
-
-//import jakarta.persistence.*;
-//
-//        import java.util.Date;
-//
-//@Entity
-//public class Student {
-//    @Id
-//    private long id;
-//
-//    @Column(name = "student_name")
-//    private String studentName;
-//
-//    @Column(name = "roll_number", nullable = false)
-//    private int rollNumber;
-//
-//    @Temporal(TemporalType.DATE)
-//    private Date birthDate;
-//
-//    @Transient
-//    @Column(name = "full_name", nullable = false)
-//    private String fullName;
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public String getStudentName() {
-//        return studentName;
-//    }
-//
-//    public void setStudentName(String studentName) {
-//        this.studentName = studentName;
-//    }
-//
 //    public int getRollNumber() {
 //        return rollNumber;
 //    }
@@ -109,5 +33,5 @@ public class Student {
 //    public void setRollNumber(int rollNumber) {
 //        this.rollNumber = rollNumber;
 //    }
-//}
 
+}
